3a2346155de0e379c3008bee51b53bb22e571eef
switch-blade-stuff <terekhin.mikhail.01@gmail.com>
2023-03-12 07:21:42 -0700

    Added tests for matrix transform functions

--- test/main.cpp
+++ test/main.cpp
@@ -6,6 +6,18 @@
 
 #define TEST_ASSERT(cnd) DPM_ASSERT_ALWAYS(cnd)
 
+inline void test_translate() noexcept
+{
+	const auto invoke_test = [](sek::vec3<float> v, sek::vec3<float> delta, sek::vec3<float> expected)
+	{
+		const auto m = sek::translate(sek::mat4x4<float>::identity(), delta);
+		TEST_ASSERT(sek::fcmp_eq((m * sek::vec4<float>{v, 1}).xyz(), expected));
+	};
+
+	invoke_test({1, 0, 0}, {1, 1, 1}, {2, 1, 1});
+	invoke_test({1, 0, 0}, {-1, 1, 1}, {0, 1, 1});
+}
+
 inline void test_rotate() noexcept
 {
 	const auto invoke_test = [](sek::vec3<float> v, float angle, sek::vec3<float> axis, sek::vec3<float> expected)
@@ -18,7 +30,7 @@ inline void test_rotate() noexcept
 		const auto m41 = rotate(sek::mat4x4<float>::identity(), -angle, axis);
 		const auto q1 = sek::quat<float>::angle_axis(angle, -axis);
 
-		auto v0 = (m40 * sek::vec4<float>{v, 1}).xyz();
+		auto v0 = (m40 * sek::vec4<float>{v, 0}).xyz();
 		auto v1 = m30 * v;
 		auto v2 = q0 * v;
 
@@ -51,9 +63,33 @@ inline void test_rotate() noexcept
 	invoke_test(sek::vec3<float>::down(), sek::rad(-90.0f), sek::vec3<float>::forward(), sek::vec3<float>::right());
 }
 
+inline void test_scale() noexcept
+{
+	const auto invoke_test = [](sek::vec3<float> v, sek::vec3<float> factor, sek::vec3<float> expected)
+	{
+		const auto m40 = sek::scale(sek::mat4x4<float>::identity(), factor);
+		const auto m30 = sek::scale(sek::mat3x3<float>::identity(), factor);
+		const auto v0 = (m40 * sek::vec4<float>{v, 1}).xyz();
+		const auto v1 = m30 * v;
+
+		TEST_ASSERT(sek::fcmp_eq(v0, expected));
+		TEST_ASSERT(sek::fcmp_eq(v0, v1));
+	};
+
+	invoke_test({1, 0, 0}, {2, 2, 2}, {2, 0, 0});
+	invoke_test({1, 1, 0}, {2, 2, 2}, {2, 2, 0});
+	invoke_test({1, 1, 1}, {2, 2, 2}, {2, 2, 2});
+	invoke_test({1, 1, 1}, {1, 1, 1}, {1, 1, 1});
+	invoke_test({1, 1, 1}, {2, 1, 1}, {2, 1, 1});
+	invoke_test({1, 1, 1}, {2, 2, 1}, {2, 2, 1});
+	invoke_test({0, 0, 0}, {2, 2, 2}, {0, 0, 0});
+}
+
 int main()
 {
 	TEST_ASSERT((sek::mat4x4<float>::identity() == sek::mat4x4<float>{sek::mat3x3<float>::identity(), sek::vec3<float>{0}}));
 
+	test_translate();
 	test_rotate();
+	test_scale();
 }
